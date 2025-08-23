896d11e4205c8033b8903247fb7f2eed74d6bc8a
willkl2415 <68011693+willkl2415@users.noreply.github.com>
2025-08-23 16:36:30 +0100

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,11 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        black: "#0D0D0D",
-        white: "#FFFFFF",
-        offwhite: "#F9F9F9",
-        blue: "#4A90E2",
-        grey: "#7D7D7D",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
